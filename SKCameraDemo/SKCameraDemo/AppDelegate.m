df683f97b2ffc6fd8c466eee2701729162b4eb54
KUN <wushangkun@youqingxu.com>
2017-05-22 17:23:17 +0800

    use synx

--- SKCameraDemo/SKCameraDemo/AppDelegate.m
+++ SKCameraDemo/SKCameraDemo/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "RecordViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -17,6 +19,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[RecordViewController new]];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
---
+++ SKCameraDemo/SKCameraDemo/Main/Controllers/RecordViewController.m
@@ -0,0 +1,27 @@
+//
+//  RecordViewController.m
+//  SKCameraDemo
+//
+//  Created by KUN on 17/5/22.
+//  Copyright © 2017年 lemon. All rights reserved.
+//
+
+#import "RecordViewController.h"
+
+@interface RecordViewController ()
+
+@end
+
+@implementation RecordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"录制";
+    self.view.backgroundColor = [UIColor colorWithWhite:1 alpha:0.5];
+}
+
+
+
+
+@end
